c18c3db4e8b84a94bcfdf0034741d208670c5abd
HuynhThienLuan <luancm20v7x017@dttx.ctu.edu.vn>
2022-07-08 22:31:13 +0700

    Cai dat xu ly loi

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const app = require("./app");
 const config = require("./config");
 
+
+
+
 const PORT = config.app.port;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+    
 
-//
+
